d8c8ed8c60f4fc3f09a38bb6e09c19ced21656d2
Jonathan Thom and Tyler Ferrin <jonathan+tyler@>
2017-02-07 09:07:33 -0800

    previous lesson spec passes

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,8 @@
 class Lesson < ActiveRecord::Base
   validates :name, :number, :content, :presence => true
+
+  def self.lessonPrev(lessonNumber)
+    prevLesson = lessonNumber - 1
+    Lesson.where(number: prevLesson)
+  end
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -4,4 +4,14 @@ describe Lesson do
   it { should validate_presence_of :number}
   it { should validate_presence_of :name}
   it { should validate_presence_of :content}
+
+  describe('.lessonPrev') do
+    it ('returns the previous lesson') do
+      lesson1 = Lesson.new(name: "Lesson1", content: "Lorem Ipsum", number: 1)
+      lesson1.save
+      lesson2 = Lesson.new(name: "Lesson2", content: "Lorem Ipsum", number: 2)
+      lesson2.save
+      expect(Lesson.lessonPrev(lesson2.number)).to eq([lesson1])
+    end
+  end
 end
