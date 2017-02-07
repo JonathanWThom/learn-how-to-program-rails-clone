2488bc51680bbb127bc77aaae295ec538ff878b5
Jonathan Thom and Tyler Ferrin <jonathan+tyler@>
2017-02-07 09:12:31 -0800

    next lesson method

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,6 +3,13 @@ class Lesson < ActiveRecord::Base
 
   def self.lessonPrev(lessonNumber)
     prevLesson = lessonNumber - 1
-    Lesson.where(number: prevLesson)
+    foundLesson = Lesson.where(number: prevLesson)
+    return foundLesson[0]
+  end
+
+  def self.lessonNext(lessonNumber)
+    nextLesson = lessonNumber + 1
+    foundLesson = Lesson.where(number: nextLesson)
+    return foundLesson[0]
   end
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -11,7 +11,17 @@ describe Lesson do
       lesson1.save
       lesson2 = Lesson.new(name: "Lesson2", content: "Lorem Ipsum", number: 2)
       lesson2.save
-      expect(Lesson.lessonPrev(lesson2.number)).to eq([lesson1])
+      expect(Lesson.lessonPrev(lesson2.number)).to eq(lesson1)
+    end
+  end
+
+  describe('.lessonNext') do
+    it ('returns the previous lesson') do
+      lesson1 = Lesson.new(name: "Lesson1", content: "Lorem Ipsum", number: 1)
+      lesson1.save
+      lesson2 = Lesson.new(name: "Lesson2", content: "Lorem Ipsum", number: 2)
+      lesson2.save
+      expect(Lesson.lessonNext(lesson1.number)).to eq(lesson2)
     end
   end
 end
