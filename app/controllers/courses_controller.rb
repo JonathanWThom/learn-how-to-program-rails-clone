ae3a05c59d11f603745f0a281bf11f006a8b0d69
Jonathan Thom and Tyler Ferrin <jonathan+tyler@>
2017-02-07 11:59:51 -0800

    added crud for Course model

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,6 +5,45 @@ class CoursesController < ApplicationController
 
   def show
     @course = Course.find(params[:id])
-    
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.create(course_params)
+    if @course.save
+      redirect_to courses_path
+    else
+      flash[:notice] = "Nice try ding dong. try again"
+      render :new
+    end
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    @course.update(course_params)
+    if @course.save
+      redirect_to course_path(@course)
+    else
+      flash[:notice] = "Nice try ding dong. try again"
+      render :edit
+    end
+  end
+
+  def destroy
+    @course = Course.find(params[:id])
+    @course.destroy
+    redirect_to courses_path
+  end
+
+private
+  def course_params
+    params.require(:course).permit(:name)
   end
 end
