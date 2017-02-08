d608b0624484c3441d35a733d8c1ced1d06008e0
Jonathan Thom and Tyler Ferrin <jonathan+tyler@>
2017-02-07 16:09:58 -0800

    pretty dang good coverage on these specs

--- spec/features/navigate_lesson_pages_spec.rb
+++ spec/features/navigate_lesson_pages_spec.rb
@@ -1,26 +1,24 @@
 require 'rails_helper'
 
-# describe 'the go forward and back between lessons path' do
-#   it 'will navigate to the previous lesson' do
-#     lesson1 = Lesson.create(name: 'Lesson1', content: 'Lorem Ipsum', number: 1)
-#     lesson2 = Lesson.create(name: 'Lesson2', content: 'Lorem Ipsum', number: 2)
-#     visit lesson_path(lesson2)
-#     click_link 'Previous'
-#     expect(page).to have_content('Lesson1')
-#   end
-#
-#   it 'will navigate to the next lesson' do
-#     lesson1 = Lesson.create(name: 'Lesson1', content: 'Lorem Ipsum', number: 1)
-#     lesson2 = Lesson.create(name: 'Lesson2', content: 'Lorem Ipsum', number: 2)
-#     visit lesson_path(lesson1)
-#     click_link 'Next'
-#     expect(page).to have_content('Lesson2')
-#   end
-#
-#   it 'will return to lessons page' do
-#     lesson1 = Lesson.create(name: 'Lesson1', content: 'Lorem Ipsum', number: 1)
-#     visit lesson_path(lesson1)
-#     click_link 'Return to Lessons'
-#     expect(page).to have_content('Lessons')
-#   end
-# end
+describe "navigate lessons path" do
+  it "navigates to the next lesson" do
+    course = Course.create(name: 'Rails')
+    week = course.weeks.create(name: 'Rails Basics')
+    lesson = week.lessons.create(name: 'Build a rails app', content: 'Lorem Ipsum Butts Butts Butts', number: 1)
+    lesson2 = week.lessons.create(name: 'Build a butts app', content: 'Lorem Ipsum Butts Butts Butts', number: 2)
+    visit week_lesson_path(week, lesson)
+    click_link "Next"
+    expect(page).to have_content("Build a butts app")
+  end
+
+
+  it "navigates to the previous lesson" do
+    course = Course.create(name: 'Rails')
+    week = course.weeks.create(name: 'Rails Basics')
+    lesson = week.lessons.create(name: 'Build a rails app', content: 'Lorem Ipsum Butts Butts Butts', number: 1)
+    lesson2 = week.lessons.create(name: 'Build a butts app', content: 'Lorem Ipsum Butts Butts Butts', number: 2)
+    visit week_lesson_path(week, lesson2)
+    click_link "Previous"
+    expect(page).to have_content("Build a rails app")
+  end
+end
